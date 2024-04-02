b88826e9433866828a81b10e7f084ce7db56a0ea
Alix Cook <alix.cook11@gmail.com>
2024-04-02 09:06:31 -0700

    fix

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: release
 release:
-	docker run -it -v `pwd`:`pwd` \
-		--workdir `pwd` -e GITHUB_TOKEN -e USER \
-		--entrypoint scripts/release.sh \
+	docker run -it -v `pwd`/scripts/release.sh:/workdir/release.sh \
+		--workdir /workdir -e GITHUB_TOKEN -e USER \
+		--entrypoint /workdir/release.sh \
 		quay.io/helmpack/chart-releaser:v1.6.1
--- scripts/release.sh
+++ scripts/release.sh
@@ -16,9 +16,13 @@ fi
 rm -rf .cr-release-packages || true
 rm index.yaml || true
 
+
 git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 git config --global user.email "${USER}@metronome.com"
 git config --global user.name "${USER}"
+
+git clone https://github.com/Metronome-Industries/metronome-charts
+cd metronome-charts
 
 cr package ./aws-load-balancer-controller
 cr upload --owner Metronome-Industries --git-repo metronome-charts --packages-with-index --token $GITHUB_TOKEN --push --skip-existing
