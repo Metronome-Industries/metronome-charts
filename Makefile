af1560acf90f5ae1fdde62d786a1bf7a1af57a55
Alix Cook <alix.cook11@gmail.com>
2024-04-02 09:03:07 -0700

    fix

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 .PHONY: release
 release:
 	docker run -it -v `pwd`:`pwd` \
-		--workdir `pwd` -e GITHUB_TOKEN \
-		--entrypoint scripts/release.sh quay.io/helmpack/chart-releaser:v1.6.1
+		--workdir `pwd` -e GITHUB_TOKEN -e USER \
+		--entrypoint scripts/release.sh \
+		quay.io/helmpack/chart-releaser:v1.6.1
--- scripts/release.sh
+++ scripts/release.sh
@@ -10,10 +10,15 @@ fatal() {
 if [[ -z "${GITHUB_TOKEN}" ]] ; then
     fatal "Missing GITHUB_TOKEN env variable"
 fi
+if [[ -z "${USER}" ]] ; then
+    fatal "USER not set"
+fi
 rm -rf .cr-release-packages || true
 rm index.yaml || true
 
 git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+git config --global user.email "${USER}@metronome.com"
+git config --global user.name "${USER}"
 
 cr package ./aws-load-balancer-controller
 cr upload --owner Metronome-Industries --git-repo metronome-charts --packages-with-index --token $GITHUB_TOKEN --push --skip-existing
